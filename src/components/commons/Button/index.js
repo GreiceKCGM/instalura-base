9d879d1afb6750f2babe59c92d9c512e8c8a3120
Greice Kelly <greicekellybc@gmail.com>
2021-02-15 22:13:16 -0300

    aula03

---
+++ src/components/commons/Button/index.js
@@ -0,0 +1,42 @@
+import styled, { css } from 'styled-components';
+import get from 'lodash/get';
+
+const ButtonGhost = css`
+    color: ${(props) => get (props.theme,`colors.${props.variant}.color`)
+    };
+    background: transparent;
+`
+
+const ButtonDefault = css`
+    color: white;
+    background-color: ${function(props) {
+        return get(props.theme, `colors.${props.variant}.color`)
+    }};
+    color: ${function(props) {
+        return get (props.theme,`colors.${props.variant}.contrastText`)
+    }};
+`;
+
+export const Button = styled.button`
+    border: 0;
+    cursor: pointer;
+    padding: 12px 26px;
+    font-weight: bold;
+    opacity: 1;
+    border-radius: 8px;
+    ${function(props) {
+        // console.log('<Button/>', props.variant, props.theme);
+        if(props.ghost) {
+            return ButtonGhost;
+        }
+        return ButtonDefault
+    }}
+    transition: opacity ${({ theme }) => theme.transition};
+    border-radius: ${({ props }) => props.theme.borderRadius};
+    /* ${({ ghost }) => (ghost ? ButtonGhost : ButtonDefault)} */
+    &:hover,
+    &:focus {
+      opacity: .5;
+    }
+
+`;
--- src/components/commons/Menu/index.js
+++ src/components/commons/Menu/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Logo } from '../../../theme/logo';
+import { Button } from '../Button';
 import { MenuWrapper } from './styles/MenuWrapper';
 
 const links = [
@@ -36,12 +37,12 @@ export default function Menu(){
         ))}
       </MenuWrapper.CentralSide>
       <MenuWrapper.RigthSide>
-        <button>
+        <Button ghost variant="secondary.main">
           Entrar
-        </button>
-        <button>
+        </Button>
+        <Button variant="primary.main">
           Cadastrar
-        </button>
+        </Button>
       </MenuWrapper.RigthSide>
 
     </MenuWrapper>
