ad8372f40faf58a0542c4f40fcdb55df3fd0332c
Greice Kelly <greicekellybc@gmail.com>
2021-02-17 15:43:50 -0300

    alterações no index text- tentando corrigir o index page

--- src/components/foundation/text/index.js
+++ src/components/foundation/text/index.js
@@ -1,27 +1,47 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import get from 'lodash/get';
-import styled, { css } from 'styled-components';
+import styled,{ css } from 'styled-components';
 import { propToStyle } from '../../../theme/utils/propToStyle';
+import { breakpointsMedia } from '../../../theme/utils/breakpointsMedia';
 
+
+const paragraph1 = css`
+  ${({ theme }) => css`
+    font-size: ${theme.typographyVariants.paragraph1.fontSize};
+    font-weight: ${theme.typographyVariants.paragraph1.fontWeight};
+    line-height: ${theme.typographyVariants.paragraph1.lineHeight};
+  `}
+`;
+
+const smallestException = css`
+  ${({ theme }) => css`
+    font-size: ${theme.typographyVariants.smallestException.fontSize};
+    font-weight: ${theme.typographyVariants.smallestException.fontWeight};
+    line-height: ${theme.typographyVariants.smallestException.lineHeight};
+  `}
+`;
 
 export const TextStyleVariants = {
-    paragraph1: css`
-        font-size: ${({ theme }) => theme.typographyVariants.paragraph1.fontiSize};
-        font-Weight: ${({ theme }) => theme.typographyVariants.paragraph1.fontiWeight};
-        line-height: ${({ theme }) => theme.typographyVariants.paragraph1.lineHeight};
-
+    smallestException,
+    paragraph1,
+    title: css`
+      ${({ theme }) => css`
+        font-size: ${theme.typographyVariants.titleXS.fontSize};
+        font-weight: ${theme.typographyVariants.titleXS.fontWeight};
+        line-height: ${theme.typographyVariants.titleXS.lineHeight};
+      `}
+      ${breakpointsMedia({
+        md: css`
+          ${({ theme }) => css`
+            font-size: ${theme.typographyVariants.title.fontSize};
+            font-weight: ${theme.typographyVariants.title.fontWeight};
+            line-height: ${theme.typographyVariants.title.lineHeight};
+          `}
+        `,
+      })}
     `,
-
-    smallestException: css`
-        font-size: ${({ theme }) => theme.typographyVariants.smallestException.fontiSize};
-        font-Weight: ${({ theme }) => theme.typographyVariants.smallestException.fontiWeight};
-        line-height: ${({ theme }) => theme.typographyVariants.smallestException.lineHeight};
-
-    `,
-}
-
-
+  };
 
 
 const TextBase = styled.span `
