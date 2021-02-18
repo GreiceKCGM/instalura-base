9207774557ccf07a3ea1b45a270af67d1e9cf29a
Greice Kelly <greicekellybc@gmail.com>
2021-02-17 22:19:53 -0300

    inclusão do title variant Proptypes

--- src/components/foundation/text/index.js
+++ src/components/foundation/text/index.js
@@ -83,7 +83,7 @@ export default function Text({ tag, variant, children, ...props }) {
 Text.propTypes = {
     children: PropTypes.node.isRequired,
     tag: PropTypes.oneOf(['h1', 'h2', 'h3', 'h4', 'h5', 'h6', 'p', 'li', 'a', 'span']),
-    variant: PropTypes.oneOf(['paragraph1', 'smallestException']),
+    variant: PropTypes.oneOf(['paragraph1', 'smallestException', 'title']),
 };
 
 Text.defaultProps = {
