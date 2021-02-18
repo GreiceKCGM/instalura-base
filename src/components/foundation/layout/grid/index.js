5965d38839f8c55ab11ea6a9e6912b38d6b9b866
Greice Kelly <greicekellybc@gmail.com>
2021-02-17 23:27:56 -0300

    informações de css no grid

--- src/components/foundation/layout/grid/index.js
+++ src/components/foundation/layout/grid/index.js
@@ -1,14 +1,55 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
+import { breakpointsMedia } from '../../../../theme/utils/breakpointsMedia';
+
+
+const Container = styled.div`
+    width: 100%;
+    padding-right: 28px;
+    padding-left: 28px;
+    margin-right:auto;
+    margin-left:auto;
+    max-width: initial;
+
+    ${breakpointsMedia({
+            
+        sm: css`
+            max-width: 576px;
+        `,
+        md: css`
+            max-width: 768px;
+            padding-right: 16px;
+            padding-left: 16px;
+        `,
+        lg: css`
+            max-width: 1160px;
+        `,
+        xl: css`
+            max-width: 1222px;
+        `,
+    })}
+    
+`;
+
+const Row = styled.div`
+display: flex;
+flex-wrap: wrap;
+margin-right: -16px;
+margin-left: -16px;
+
+`;
+
+const Col = styled.div`
+    padding-right: 16px;
+    padding-left: 16px;
+    flex-basis: 0;
+    flex-grow: 1;
+    max-width: 100%;
+
+`;
 
 export const Grid = {
-    Container:styled.div`
-    
-    `,
-    Row:styled.div`
-    
-    `,
-    Col:styled.div`
-    
-    `,
+    Container,
+    Row,
+    Col,
 
 };
