53ff7bcc48e292617ab54f08e010ba281dd438b7
GreiceKCGM <51801614+GreiceKCGM@users.noreply.github.com>
2021-02-25 15:56:16 -0300

    Modal melhorias (#7)
    * está dando erro
    
    * instação do framer motion
    
    * adicionando ajustes finos do Modal - aula 02
    modulo02
    
    * correção {}
    
    * correção da props

--- src/components/commons/Modal/index.js
+++ src/components/commons/Modal/index.js
@@ -1,54 +1,79 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled, { css } from 'styled-components';
+import { motion } from 'framer-motion';
 
 const ModalWrapper = styled.div`
-display: flex;
-flex-direction: column;
-align-items: stretch;
-background: rgbs(0,0,0,0.1);
-position: fixed;
-top: 0;
-left: 0;
-right: 0;
-bottom: 0;
-margin: auto;
-overflow: scroll;
+  display: flex;
+  flex-direction: column;
+  align-items: stretch;
+  background: rgbs(0, 0, 0, 0.9);
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  margin: auto;
+  overflow: scroll;
+  transition: 0.3s;
+  z-index: 100;
 
-${({ isOpen }) => {
+  ${({ isOpen }) => {
     if (isOpen) {
       return css`
-    opacity: 1;
-    pointer-events:all;`;
+        opacity: 1;
+        pointer-events: all;
+      `;
     }
     return css`
-    opacity:0;
-    pointer-events:none;
+      opacity: 0;
+      pointer-events: none;
     `;
-  }}`;
+  }}
+`;
 
 function Modal({ isOpen, onClose, children }) {
   return (
     <ModalWrapper
       isOpen={isOpen}
       onClick={(event) => {
-        const isSafeArea = event.target.closest('[data-modal-safe-area="true"]');
+        const isSafeArea = event.target.closest(
+          '[data-modal-safe-area="true"]'
+        );
         // isOpen = false;
         if (!isSafeArea) {
           onClose();
         }
       }}
-
     >
-      {children({
-        'data-modal-safe-area': 'true',
-      })}
+      <motion.div
+        variants={{
+          open: {
+            x: 0,
+          },
+          closed: {
+            x: '-100%', // quanto maior para sair da tela
+          },
+        }}
+        animate={isOpen ? 'open' : 'closed'}
+        transition={{
+          duration: 0.5,
+        }}
+        style={{
+          display: 'flex',
+          flex: 1,
+        }}
+      >
+        {children({
+          'data-modal-safe-area': 'true',
+        })}
+      </motion.div>
     </ModalWrapper>
   );
 }
 Modal.propTypes = {
   isOpen: PropTypes.bool.isRequired,
-  children: PropTypes.node.isRequired,
+  children: PropTypes.func.isRequired,
   onClose: PropTypes.func.isRequired,
 };
 export default Modal;
