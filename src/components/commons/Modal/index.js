70ad0486e76cd20cd09dd8d095b2ea1edb7773f6
Greice Kelly <greicekellybc@gmail.com>
2021-02-27 10:17:45 -0300

    correção

--- src/components/commons/Modal/index.js
+++ src/components/commons/Modal/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import PropTypes from 'prop-types';
-import styled, {createGlobalStyle, css } from 'styled-components';
+import styled, { createGlobalStyle, css } from 'styled-components';
 import { motion } from 'framer-motion';
 
 const ModalWrapper = styled.div`
