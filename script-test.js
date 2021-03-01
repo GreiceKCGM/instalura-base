da6b0000de51d1618fbad42d886b4527a0b9f192
Greice Kelly <greicekellybc@gmail.com>
2021-03-01 00:03:43 -0300

    fix lint

--- script-test.js
+++ script-test.js
@@ -1,7 +1,10 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
 const shell = require('shelljs');
 
+// eslint-disable-next-line no-console
 console.log('Ola Mundo');
 
-const resultado =shell.exec('git diff --name-only Modulo02_Aula05..main', { silent: true });
+const resultado = shell.exec('git diff --name-only Modulo02_Aula05..main', { silent: true });
 
+// eslint-disable-next-line no-console
 console.log(resultado.stdout.split('\n'));
