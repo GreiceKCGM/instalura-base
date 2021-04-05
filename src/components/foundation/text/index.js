e4b5bf3b5b4b55e939138d721a4ab85303d7705c
Greice Kelly <greicekellybc@gmail.com>
2021-04-05 18:01:03 -0300

    correção do eslint

--- src/components/foundation/text/index.js
+++ src/components/foundation/text/index.js
@@ -4,7 +4,7 @@ import get from 'lodash/get';
 import styled, { css } from 'styled-components';
 import propToStyle from '../../../theme/utils/propToStyle';
 import breakpointsMedia from '../../../theme/utils/breakpointsMedia';
-import { Link } from '../../commons/Link';
+import Link from '../../commons/Link';
 
 const paragraph1 = css`
   ${({ theme }) => css`
