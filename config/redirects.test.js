ad90cd5e04ad03a034bd56236d762d3eb77b3673
Greice Kelly <greicekellybc@gmail.com>
2021-04-07 11:18:51 -0300

    Feat: Final da Aula 04 Modulo 04
    Feito os testes no propToStyle e breackpointes.
    
    Arrumado um bug

--- config/redirects.test.js
+++ config/redirects.test.js
@@ -1,7 +1,8 @@
 import redirects from './redirects';
 
+// Test Driven Development
 describe('config/redirects', () => {
-  test('renders all current redirect', () => {
-    expect(redirects).toMatchSnapshot(); // Fotografia
+  test('renders all current redirects', () => {
+    expect(redirects).toMatchSnapshot(); // Fotografia/Insta/Polaroid
   });
 });
--- src/theme/utils/propToStyle/propToStyle.js
+++ src/theme/utils/propToStyle/propToStyle.js
@@ -1,34 +1,44 @@
 import breakpointsMedia from '../breakpointsMedia';
 
 function propToStyle(propName) {
-  return function PropToStyle(props) {
+  return (props) => {
     const propValue = props[propName];
 
     if (typeof propValue === 'string' || typeof propValue === 'number') {
       return {
+        // textAlign: props.textAlign
         [propName]: propValue,
       };
     }
+
     if (typeof propValue === 'object') {
-      return breakpointsMedia({
-        xs: {
-          [propName]: propValue.xs,
-        },
-        sm: {
-          [propName]: propValue.sm,
-        },
-        md: {
-          [propName]: propValue.md,
-        },
-        lg: {
-          [propName]: propValue.lg,
-        },
-        xl: {
-          [propName]: propValue.xl,
-        },
-      });
+      const breakpoints = {};
+
+      if (propValue.xs) breakpoints.xs = { [propName]: propValue.xs };
+      if (propValue.md) breakpoints.md = { [propName]: propValue.md };
+      if (propValue.xl) breakpoints.xl = { [propName]: propValue.xl };
+
+      return breakpointsMedia(breakpoints);
+
+      // return breakpointsMedia({
+      //   xs: {
+      //     [propName]: propValue.xs,
+      //   },
+      //   sm: {
+      //     [propName]: propValue.sm,
+      //   },
+      //   md: {
+      //     [propName]: propValue.md,
+      //   },
+      //   lg: {
+      //     [propName]: propValue.lg,
+      //   },
+      //   xl: {
+      //     [propName]: propValue.xl,
+      //   },
+      // });
     }
-    return ({});
+    return {};
   };
 }
 
