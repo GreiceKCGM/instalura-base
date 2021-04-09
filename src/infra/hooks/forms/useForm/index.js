cff0862bd56d7ab44de2f8dbffd53f8e34c15366
Greice Kelly <greicekellybc@gmail.com>
2021-04-09 15:22:55 -0300

    inserindo corres no thema, para a mensagem de erro ou success.
    códigos no hooks,
    validação do login usuario

--- src/infra/hooks/forms/useForm/index.js
+++ src/infra/hooks/forms/useForm/index.js
@@ -1,14 +1,34 @@
 import React from 'react';
 
-export function useForm({ initialValues, onSubmit }) {
+export function useForm({
+  initialValues,
+  onSubmit,
+  validateSchema,
+}) {
   const [values, setValues] = React.useState(initialValues);
 
-  const [isFormDisabled, setIsFormSubmited] = React.useState(true);
+  const [isFormDisabled, setIsFormDisabled] = React.useState(true);
+  const [errors, setErrors] = React.useState({});
+  const [touched, setTouchedFields] = React.useState({});
 
   React.useEffect(() => {
-    if (values.usuario.length > 0) {
-
-    }
+    validateSchema(values)
+      .then(() => {
+        setIsFormDisabled(false);
+        setErrors({});
+      })
+      .catch((err) => {
+        const formatedErrors = err.inner.reduce((errorObjectAcc, currentError) => {
+          const fieldName = currentError.path;
+          const errorMessage = currentError.message;
+          return {
+            ...errorObjectAcc,
+            [fieldName]: errorMessage,
+          };
+        }, {});
+        setErrors(formatedErrors);
+        setIsFormDisabled(true);
+      });
   }, [values]);
 
   return {
@@ -25,6 +45,17 @@ export function useForm({ initialValues, onSubmit }) {
         ...currentValues,
         [fieldName]: value,
       }));
+    },
+    isFormDisabled,
+    errors,
+    touched,
+    handleBlur(event) {
+      const fieldName = event.target.getAttribute('name');
+
+      setTouchedFields({
+        ...touched,
+        [fieldName]: true,
+      });
     },
   };
 }
