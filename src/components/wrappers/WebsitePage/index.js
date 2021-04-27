59d1d9062ae2c77e106c078dfd12fe0033516022
Greice Kelli <greicekellybc@gmail.com>
2021-04-27 16:25:34 -0300

    MOdulo 05 aula04

--- src/components/wrappers/WebsitePage/index.js
+++ src/components/wrappers/WebsitePage/index.js
@@ -1,5 +1,6 @@
 /* eslint-disable react/jsx-props-no-spreading */
 import React from 'react';
+import get from 'lodash/get';
 import PropTypes from 'prop-types';
 import Footer from '../../commons/Footer';
 import Menu from '../../commons/Menu';
@@ -7,16 +8,16 @@ import Modal from '../../commons/Modal';
 import Box from '../../foundation/layout/Box';
 import FormCadastro from '../../patterns/FormCadastro';
 import SEO from '../../commons/SEO';
+import { WebsitePageContext } from './context';
 
-export const WebsitePageContext = React.createContext({
-  toggleModalCadastro: () => {},
-});
+export { WebsitePageContext } from './context';
 
 export default function WebsitePageWrapper({
   children,
   seoProps,
   pageBoxProps,
   menuProps,
+  messages,
 }) {
   const [isModalOpen, setModalState] = React.useState(false);
   return (
@@ -25,6 +26,7 @@ export default function WebsitePageWrapper({
         toggleModalCadastro: () => {
           setModalState(!isModalOpen);
         },
+        getCMSContent: (cmsKey) => get(messages, cmsKey),
 
       }}
     >
@@ -72,6 +74,7 @@ WebsitePageWrapper.defaultProps = {
   menuProps: {
     display: true,
   },
+  messages: {},
 };
 
 WebsitePageWrapper.propTypes = {
@@ -87,4 +90,6 @@ WebsitePageWrapper.propTypes = {
     backgroundPosition: PropTypes.string,
   }),
   children: PropTypes.node.isRequired,
+  // eslint-disable-next-line react/forbid-prop-types
+  messages: PropTypes.object,
 };
